d19b353e1ae10ce96749cb34b5b3c0655ea36c79
Ariel Syafiqri <88129448+CincauEXE@users.noreply.github.com>
2022-03-18 19:01:49 +0700

    Update tempest-merlinx.sh

--- merlinx/tempest-merlinx.sh
+++ merlinx/tempest-merlinx.sh
@@ -6,6 +6,8 @@
 echo "Downloading few Dependecies . . ."
 # Toolchain
 git clone --depth=1 https://github.com/NusantaraDevs/DragonTC -b 10.0 dtc
+git clone --depth=1 https://github.com/ZyCromerZ/aarch64-zyc-linux-gnu -b 12 gcc
+git clone --depth=1 https://github.com/ZyCromerZ/arm-zyc-linux-gnueabi -b 12 gcc32
 
 # Kernel Sources
 git clone https://github.com/CincauEXE/kernel_redmi_mt6768 -b base-r-oss-tmpst tmpst
