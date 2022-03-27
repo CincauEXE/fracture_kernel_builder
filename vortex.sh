cfd41a873b665609766a204150cb699fbaac34e6
Ariel Syafiqri <88129448+CincauEXE@users.noreply.github.com>
2022-03-27 16:26:17 +0700

    Update vortex.sh

--- vortex.sh
+++ vortex.sh
@@ -5,7 +5,7 @@
 
 echo "Downloading few Dependecies . . ."
 # Kernel Sources
-git clone https://github.com/CincauEXE/kernel_redmi_mt6768 -b base-r-oss-vrtx kernel
+git clone https://github.com/CincauEXE/kernel_redmi_mt6768 -b base-r-oss-uv-vortex kernel
 
 # Main Declaration
 # export KERNEL_NAME=$(cat "arch/arm64/configs/$DEVICE_DEFCONFIG" | grep "CONFIG_LOCALVERSION=" | sed 's/CONFIG_LOCALVERSION="-*//g' | sed 's/"*//g' )
@@ -17,7 +17,7 @@ export KBUILD_BUILD_USER=CincauEXE # Change with your own name or else.
 export KBUILD_BUILD_HOST=Isekai # Change with your own hostname.
 CLANG_VER="$("$CLANG_ROOTDIR"/bin/clang --version)"
 LLD_VER="$("$CLANG_ROOTDIR"/bin/ld.lld --version)"
-export KBUILD_COMPILER_STRING="$CLANG_VER with $LLD_VER"
+export KBUILD_COMPILER_STRING="$CLANG_VER by CincauEXE"
 IMAGE=$(pwd)/kernel/out/arch/arm64/boot/Image.gz-dtb
 HEADCOMMITID="$(git log --pretty=format:'%h' -n1)"
 DATE=$(date +"%F-%S")
@@ -66,14 +66,6 @@ tg_post_msg "<b>KernelCompiler:</b><code>Compilation has started</code>"
 cd ${KERNEL_ROOTDIR}
 make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
 make -j$(nproc) ARCH=arm64 O=out \
-    LD_LIBRARY_PATH="${CLANG_ROOTDIR}/lib64:${LD_LIBRARY_PATH}" \
-    CC=${CLANG_ROOTDIR}/bin/clang \
-    AR=${CLANG_ROOTDIR}/bin/llvm-ar \
-    NM=${CLANG_ROOTDIR}/bin/llvm-nm \
-    OBJCOPY=${CLANG_ROOTDIR}/bin/llvm-objcopy \
-    OBJDUMP=${CLANG_ROOTDIR}/bin/llvm-objdump \
-    STRIP=${CLANG_ROOTDIR}/bin/llvm-strip \
-    LD=${CLANG_ROOTDIR}/bin/ld.lld \
     CLANG_TRIPLE=aarch64-linux-gnu- \
     CROSS_COMPILE=aarch64-zyc-linux-gnu- \
     CROSS_COMPILE_ARM32=arm-zyc-linux-gnueabi-
